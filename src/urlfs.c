529f1e1e77a4cc24889fb9146e296f81d002de3a
tmcqueen-materials <tmcqueen-materials@github.com>
2024-06-12 14:51:07 -0400

    Enable functionality even for URLs without returned content lengths, first fully functional version

--- src/urlfs.c
+++ src/urlfs.c
@@ -1,4 +1,4 @@
-#define FUSE_USE_VERSION 30
+#define FUSE_USE_VERSION 29
 #define _FILE_OFFSET_BITS 64
 
 #include <fuse.h>
@@ -33,6 +33,10 @@ F<tab>/path/to/other<tab>https://url/to/other
 #define TDIR 0
 #define TFILE 1
 
+//#define FAKE_SIZE 9223372036854775807L
+#define FAKE_SIZE 0
+#define MAX_REDIRS 64
+
 char config[PATH_MAX] = {0};
 char idxfil[PATH_MAX] = {0};
 
@@ -45,14 +49,23 @@ typedef struct index_s {
     char *file;
     char *url;
     long size;
+    char allowrr;
     int flags;
     struct index_s *next;
 } file_t;
 
 file_t *fileindex = NULL;
 
+struct hdrblock {
+    int newloc;
+    struct index_s *idx;
+};
+
 struct block {
+    size_t offset;
     size_t pos;
+    size_t buffer_size;
+    size_t buffer_pos;
     void *buffer;
 };
 
@@ -77,6 +90,7 @@ static file_t *createFileNode(const char *path, const char *url, int type) {
         file->url = strdup(url);
     }
     file->size = -1;
+    file->allowrr = 0;
     file->type = type;
     file->flags = 0;
     file->next = NULL;
@@ -140,37 +154,75 @@ static file_t *createFile(const char *path, const char *url) {
 
 static size_t getBlock(void *data, size_t size, size_t nmemb, void *userp) {
     struct block *block = (struct block *)userp;
-    memcpy(block->buffer  + block->pos, data, nmemb);
-    block->pos += nmemb;
+    size_t nmemb_new = nmemb;
+    if (block->pos + nmemb_new <= block->offset) {
+        // Do nothing, not at starting offet yet
+        block->pos += nmemb_new;
+        return nmemb;
+    } else if (block->pos < block->offset) {
+        // need to skip some bytes.
+        data += (block->offset - block->pos);
+        nmemb_new -= (block->offset - block->pos);
+        block->pos = block->offset;
+    }
+    // Copy away, up to size of buffer
+    if (block->buffer_pos + nmemb_new <= block->buffer_size) {
+        memcpy(block->buffer + block->buffer_pos, data, nmemb_new);
+        block->buffer_pos += nmemb_new;
+    } else if (block->buffer_pos < block->buffer_size) {
+        memcpy(block->buffer + block->buffer_pos, data, block->buffer_size - block->buffer_pos);
+        block->buffer_pos = block->buffer_size;
+    }
+    block->pos += nmemb_new;
     return nmemb;
 }
 
 static size_t getHeader(char *b, size_t size, size_t nitems, void *ud) {
-    struct index_s *file = (struct index_s *)ud;
+    struct hdrblock *hdr = (struct hdrblock *)ud;
+    struct index_s *file = hdr->idx;
 
     if (strncasecmp(b, "content-length: ", 16) == 0) {
         file->size = strtol(b + 16, 0, 10);
+    }
+
+    if (strncasecmp(b, "accept-ranges: bytes", 20) == 0) {
+        file->allowrr = 1;
+    }
+
+    if (strncasecmp(b, "location: ", 10) == 0) {
+        char *trim = strtok(b + 10, " \t\r\n");
+        if (file->url != NULL) free(file->url);
+        file->url = strdup(trim);
+        hdr->newloc = 1;
     }
 
     return nitems;
 }
 
 static long getFileSize(struct index_s *file) {
-    file->size = 0; // Temporary, I hope
-    CURL *curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, file->url);
-    if (hdrs)
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hdrs);
-    curl_easy_setopt(curl, CURLOPT_HEADER, 0L);
-    curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-    curl_easy_setopt(curl, CURLOPT_HEADERDATA, file);
-    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, getHeader);
-    int res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-        curl_easy_strerror(res);
+    int redirs = 0;
+    struct hdrblock hdrblock;
+    hdrblock.newloc = 1;
+    hdrblock.idx = file;
+    while (hdrblock.newloc && redirs < MAX_REDIRS) {
+        redirs++;
+        hdrblock.newloc = 0;
+        file->size = -1; // Not all URLs return a content length. So size might still be -1 after this call.
+        CURL *curl = curl_easy_init();
+        curl_easy_setopt(curl, CURLOPT_URL, file->url);
+        if (hdrs)
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hdrs);
+        curl_easy_setopt(curl, CURLOPT_HEADER, 0L);
+        curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 0L); // we manually chase location to avoid spurious lengths from a 30x redirect return
+        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &hdrblock);
+        curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, getHeader);
+        int res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            curl_easy_strerror(res);
+        }
+        curl_easy_cleanup(curl);
     }
-    curl_easy_cleanup(curl);
     return 0;
 }
 
@@ -244,7 +296,11 @@ static int fuse_getattr(const char *path, struct stat *st) {
             if (file->size == -1) {
                 getFileSize(file);
             }
-            st->st_size = file->size;
+            if (file->size == -1) {
+                st->st_size = FAKE_SIZE;
+            } else {
+                st->st_size = file->size;
+            }
         }
         return 0;
     }
@@ -257,6 +313,11 @@ static int fuse_open(const char *path, struct fuse_file_info *fi) {
 
     if (file == NULL)
         return -ENOENT;
+
+    if (file->size == -1)
+        getFileSize(file);
+    if (file->size == -1)
+        fi->direct_io = 1; // must use direct IO for files of unknown length to make sure reader gets all bytes requested
 
     return 0;
 }
@@ -276,14 +337,21 @@ static int fuse_read( const char *path, char *buffer, size_t size, off_t offset,
 
     struct block block;
 
-    block.buffer = buffer;
+    if (file->allowrr)
+        block.offset = 0;
+    else
+        block.offset = offset;
     block.pos = 0;
+    block.buffer_size = size;
+    block.buffer = buffer;
+    block.buffer_pos = 0;
 
     CURL *curl = curl_easy_init();
     curl_easy_setopt(curl, CURLOPT_URL, file->url);
     if (hdrs)
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hdrs);
-    curl_easy_setopt(curl, CURLOPT_RANGE, range);
+    if (file->allowrr)
+        curl_easy_setopt(curl, CURLOPT_RANGE, range);
     curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &block);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, getBlock);
@@ -292,7 +360,7 @@ static int fuse_read( const char *path, char *buffer, size_t size, off_t offset,
         curl_easy_strerror(res);
     }
     curl_easy_cleanup(curl);
-    return block.pos;
+    return block.buffer_pos;
 }
 
 static int fuse_listxattr(const char *path, char *buffer, size_t len) {
@@ -364,7 +432,8 @@ static int fuse_statfs(const char *path, struct statvfs *st) {
             if (scan->size == -1) {
                 getFileSize(scan);
             }
-            size += scan->size;
+            if (scan->size >= 0)
+                size += scan->size;
             count++;
         }
     }
@@ -395,7 +464,6 @@ static void fuse_load_index() {
         char *type = strtok(entry, "\t\r\n");
         char *name = strtok(NULL, "\t\r\n");
         char *url = strtok(NULL, "\t\r\n");
-        char *size = strtok(NULL, "\t\r\n");
 
         if (name == NULL) continue;
 
@@ -413,9 +481,6 @@ static void fuse_load_index() {
                 file = createFile(name, url);
             }
 
-            if (size != NULL) {
-                file->size = strtoul(size, NULL, 10);
-            }
             file->flags |= F_KEEP;
         } else if (type[0] == 'D') {
             if (file != NULL) {
@@ -452,6 +517,7 @@ static void fuse_load_config() {
     char entry[32768];
     while (fgets(entry, 32768, f) != NULL) {
         char *val = strtok(entry, "\t\r\n");
+        if (val == NULL) continue;
         if (!first && strlen(val) > 0 && val[0] != '#') {
             strcpy(idxfil, val);
             first = 1;
